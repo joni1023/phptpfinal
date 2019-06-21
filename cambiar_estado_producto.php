08529714eee5ddb5c8801a338edda30ebd27b4c2
sergio-demarco <demarcosergio1@gmail.com>
2019-06-21 00:22:00 -0300

    Nuevo branch para usar pagina con estilos

--- cambiar_estado_producto.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$id = $_POST['id'];
-$estado = $_POST['estado'];
-$db = mysqli_connect("localhost", "root", "", "tp_pw");
-$sql = "UPDATE item SET estado='$estado' WHERE id='$id'";
-mysqli_query($db, $sql);
