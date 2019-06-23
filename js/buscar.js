14dd37339ca29b7d648aa22b310ee0b7a4736655
joni1023 <jonfu1023@gmail.com>
2019-06-23 13:22:12 -0300

    header

--- js/buscar.js
+++ js/buscar.js
@@ -1,19 +1,33 @@
+
 $(document).ready(function () {
-    let array=[];
-    $.ajax({
-        type: "GET",
-        dataType: "json",
-        url: 'buscaritem.php',
-        success: function (respuesta) {
-            // array = JSON.parse(respuesta);
-            console.log(respuesta);
-        }
+    console.log('Script cargado');
+    $("#buscar").keyup(function () {
+       let buscar=$("#buscar").val();
+       let buscarpor=$("#buscarpor").val();
+        console.log(buscar);
+        $.ajax({
+            url:"buscarbase.php",
+            type:"POST",
+            data:{buscar:buscar, buscarpor:buscarpor},
+            success:  function (respuesta) {
 
-    })
+                let resultados= JSON.parse(respuesta);
+                let template = '';//estocrea etiquetas
+                console.log(resultados);
 
-    $("#buscar").autocomplete({
-        source: array
-        }
-    )
+                resultados.forEach( item =>{
 
-});
+                    template += `
+                     <option value="${item.nombre} ">
+                    `
+
+                });
+                $("#buscart").html(template);
+                $('#buscados').html(template);
+
+            }
+                })
+
+        })
+
+    });
